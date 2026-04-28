ff6fb48454b0f70db47c17c97820ea3f65759865
flc043 <37825968+flc043@users.noreply.github.com>
2026-04-29 01:08:31 +0800

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'tabata-timer-pro-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+  // 如果有其他 CSS, JS, 圖片等資源，也請在此處列出
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
